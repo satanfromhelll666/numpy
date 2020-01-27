4f81c884713b5d09007bddb5942ab03ba2cf9eba
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-01-28 01:13:34 +0600

    Create max_and_min_from_a_2d_array.py

---
+++ max_and_min_from_a_2d_array.py
@@ -0,0 +1,12 @@
+import numpy as np
+n = np.array([[1,2,3,4],[5,6,7,8]])
+
+np.max(n,axis=0)
+np.min(n,axis=1)
+
+'''
+if axis = 0 then it means which row has max or min value
+if axis = 1 then it means which row and coloum has max or min value
+
+
+'''
